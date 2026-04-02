4489317f264334ffa77bea4cf10738ea4b859d74
Anasxon Inoyatov <anasxoninoyatov3@gmail.com>
2026-04-02 15:56:05 +0500

    third commit

--- app.js
+++ app.js
@@ -1,27 +1,30 @@
 const btn = document.getElementById("btn");
 const heading = document.getElementById("heading");
 btn.addEventListener("click", () => {
-    const input = prompt("1 dan 100 gacha son yozing:");
-    if (!input)
-        return;
-    const userNumber = Number(input);
-    if (isNaN(userNumber) || userNumber < 1 || userNumber > 100) {
-        heading.textContent = "Faqat 1 dan 100 gacha son yozing!";
-        return;
-    }
     let randomNumber;
     do {
         randomNumber = Math.floor(Math.random() * 100) + 1;
     } while (randomNumber === 50);
-    console.log(randomNumber);
-    if (userNumber === randomNumber) {
-        heading.textContent = "Toptiz!";
-    }
-    else if (randomNumber > 50) {
-        heading.textContent = "Random son 50 dan katta!";
-    }
-    else {
-        heading.textContent = "Random son 50 dan kichik!";
+    let userNumber = null;
+    while (userNumber !== randomNumber) {
+        const input = prompt("1 dan 100 gacha son yozing:");
+        if (!input)
+            return;
+        userNumber = Number(input);
+        if (isNaN(userNumber) || userNumber < 1 || userNumber > 100) {
+            alert("Faqat 1 dan 100 gacha son yozing!");
+            continue;
+        }
+        if (userNumber === randomNumber) {
+            heading.textContent = "Toptiz!";
+            break;
+        }
+        else if (randomNumber > 50) {
+            alert("Random son 50 dan katta!");
+        }
+        else {
+            alert("Random son 50 dan kichik!");
+        }
     }
 });
 export {};
--- app.ts
+++ app.ts
@@ -2,27 +2,31 @@ const btn = document.getElementById("btn") as HTMLButtonElement;
 const heading = document.getElementById("heading") as HTMLHeadingElement;
 
 btn.addEventListener("click", () => {
-  const input = prompt("1 dan 100 gacha son yozing:");
-  if (!input) return;
-
-  const userNumber = Number(input);
-
-  if (isNaN(userNumber) || userNumber < 1 || userNumber > 100) {
-    heading.textContent = "Faqat 1 dan 100 gacha son yozing!";
-    return;
-  }
-
   let randomNumber: number;
   do {
     randomNumber = Math.floor(Math.random() * 100) + 1;
   } while (randomNumber === 50);
-  console.log(randomNumber);
 
-  if (userNumber === randomNumber) {
-    heading.textContent = "Toptiz!";
-  } else if (randomNumber > 50) {
-    heading.textContent = "Random son 50 dan katta!";
-  } else {
-    heading.textContent = "Random son 50 dan kichik!";
+  let userNumber: number | null = null;
+
+  while (userNumber !== randomNumber) {
+    const input = prompt("1 dan 100 gacha son yozing:");
+    if (!input) return;
+
+    userNumber = Number(input);
+
+    if (isNaN(userNumber) || userNumber < 1 || userNumber > 100) {
+      alert("Faqat 1 dan 100 gacha son yozing!");
+      continue;
+    }
+
+    if (userNumber === randomNumber) {
+      heading.textContent = "Toptiz!";
+      break;
+    } else if (randomNumber > 50) {
+      alert("Random son 50 dan katta!");
+    } else {
+      alert("Random son 50 dan kichik!");
+    }
   }
 });
